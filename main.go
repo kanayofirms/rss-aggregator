740095de241bb3f52e09a5d6f5d096555755a8c3
kanayofirms <kanyeze@gmail.com>
2023-08-14 17:07:38 +0100

    Create new router and spin up server for connection to http

--- main.go
+++ main.go
@@ -4,19 +4,33 @@ import (
 	"log"
 	"fmt"
 	"os"
+	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:	":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 
 	fmt.Println("Port:", portString)
 }
